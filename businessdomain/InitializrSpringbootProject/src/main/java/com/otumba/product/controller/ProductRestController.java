8f3ac5dc8fc5593d042989ca96436dd8a8b21e5d
Pedro Coronel Santeliz <pelsanmexico@gmail.com>
2022-12-03 08:42:58 -0600

    feat: adding external configuration

--- businessdomain/InitializrSpringbootProject/src/main/java/com/otumba/product/controller/ProductRestController.java
+++ businessdomain/InitializrSpringbootProject/src/main/java/com/otumba/product/controller/ProductRestController.java
@@ -1,20 +1,34 @@
 package com.otumba.product.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.otumba.product.back.DemoProperties;
 import com.otumba.product.entities.Product;
 import com.otumba.product.repository.ProductRepository;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import java.time.Duration;
+import java.util.Collections;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  *
@@ -27,10 +41,12 @@ public class ProductRestController {
     @Autowired
     ProductRepository productRepository;
 
+    private final WebClient.Builder webClientBuilder;
     private final DemoProperties properties;
 
     @Autowired
-    public ProductRestController(DemoProperties properties) {
+    public ProductRestController(DemoProperties properties, WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
         this.properties = properties;
     }
 
@@ -42,6 +58,41 @@ public class ProductRestController {
 
         return "Hello World!";
     }
+
+    @GetMapping("/check")
+    public String check() {
+
+        WebClient build = webClientBuilder.clientConnector(new ReactorClientHttpConnector(client))
+                .baseUrl("http://run.mocky.io/v2/5185415ba171ea3a00704eed")
+                // Setting headers
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://run.mocky.io/v2/5185415ba171ea3a00704eed"))
+                .build();
+
+        // Setting call get product by id
+        JsonNode block = build.method(HttpMethod.GET).uri("/")
+                .retrieve().bodyToMono(JsonNode.class).block();
+
+        System.out.println("json" + block.toPrettyString());
+        
+        return block.toPrettyString();
+    }
+
+    //webClient requires HttpClient library to work propertly       
+    HttpClient client = HttpClient.create()
+            //Connection Timeout: is a period within which a connection between a client and a server must be established
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            //Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read and Write Timeout: A read timeout occurs when no data was read within a certain 
+            //period of time, while the write timeout when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
 
     @GetMapping()
     public List<Product> list() {
