1b3c8d7a4ead2b2697ff6770dca12d89df389121
peter <pelsanmexico@gmail.com>
2022-12-16 09:13:54 -0600

    feat: added to cliente microservice contection to postgres database

---
+++ businessdomain/cliente/src/main/java/com/otumba/cliente/controller/ClienteRestController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.otumba.cliente.controller;
+
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author peter
+ */
+@RestController
+@RequestMapping("/dog")
+public class ClienteRestController {
+
+    @GetMapping("/test")
+    public String get(@RequestHeader Map<String, String> headers) {
+        headers.forEach((key, value) -> {
+            System.out.println(String.format("Header '%s' = %s", key, value));
+        });
+        return "OK";
+    }
+}
---
+++ businessdomain/cliente/src/main/java/com/otumba/cliente/entities/Cliente.java
@@ -0,0 +1,35 @@
+package com.otumba.cliente.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ *
+ * @author peter
+ */
+@Entity
+@Table(name="clientes")
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+    
+    private String nombre;
+    private String apellido;
+    private String email;
+    
+    @Column(name="create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+}
