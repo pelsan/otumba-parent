d5c8ba2d570b5dd7d44576b9d7696d96a158d90b
Pedro Coronel Santeliz <pelsanmexico@gmail.com>
2022-12-21 12:13:34 -0600

    feat: added update and delete methos to api clientes

--- businessdomain/cliente/src/main/java/com/otumba/cliente/controller/ClienteRestController.java
+++ businessdomain/cliente/src/main/java/com/otumba/cliente/controller/ClienteRestController.java
@@ -12,9 +12,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -62,6 +64,22 @@ public class ClienteRestController {
        // cliente.setCreateAt(new Date());
        
         return clienteService.save(cliente);
+    }
+    
+    @PutMapping ("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update (@RequestBody Cliente cliente, @PathVariable Long id) {
+        Cliente clienteActual = clienteService.findById(id);
+        clienteActual.setApellido(cliente.getNombre());
+        clienteActual.setEmail(cliente.getEmail());
+        clienteActual.setNombre(cliente.getNombre());
+        return clienteService.save(clienteActual);
+    }
+    
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete (@PathVariable Long id) {
+        clienteService.delete(id);
     }
     
 }
