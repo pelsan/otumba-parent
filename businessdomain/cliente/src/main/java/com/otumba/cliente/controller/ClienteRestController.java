fb09c5eca6fa0f89955e35a5552d4e584bd2c1fc
Pedro Coronel Santeliz <pelsanmexico@gmail.com>
2022-12-21 14:31:24 -0600

    feat: added error messages on show , not found o error on database

--- businessdomain/cliente/src/main/java/com/otumba/cliente/controller/ClienteRestController.java
+++ businessdomain/cliente/src/main/java/com/otumba/cliente/controller/ClienteRestController.java
@@ -7,10 +7,13 @@ package com.otumba.cliente.controller;
 import com.otumba.cliente.entities.Cliente;
 import com.otumba.cliente.services.IClienteService;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,9 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author peter
  */
-
 // va a permitir a la aplicacion angular conectarse al servicio
-@CrossOrigin(origins={"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/cliente")
 public class ClienteRestController {
@@ -47,39 +49,55 @@ public class ClienteRestController {
 
     @GetMapping("/clientes")
     public List<Cliente> index() {
-           return clienteService.findAll();
+        return clienteService.findAll();
     }
-    
+
     @GetMapping("clientes/{id}")
-    public Cliente show (@PathVariable Long id){
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            cliente = clienteService.findById(id);
+
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al consultar la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" No existe en base")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
-    
+
     @PostMapping("/clientes")
     // por default contesta 200 pero en este caso queremos responder un codigo 201
-    @ResponseStatus (HttpStatus.CREATED)
-    public Cliente create (@RequestBody Cliente cliente){
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente) {
         // lo siguiente se puede poner para asignar fecha pero mejor se va a asignar al prepersistir dentro de el entity
         // en la clase cliente
-       // cliente.setCreateAt(new Date());
-       
+        // cliente.setCreateAt(new Date());
+
         return clienteService.save(cliente);
     }
-    
-    @PutMapping ("/clientes/{id}")
+
+    @PutMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update (@RequestBody Cliente cliente, @PathVariable Long id) {
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
         Cliente clienteActual = clienteService.findById(id);
         clienteActual.setApellido(cliente.getNombre());
         clienteActual.setEmail(cliente.getEmail());
         clienteActual.setNombre(cliente.getNombre());
         return clienteService.save(clienteActual);
     }
-    
+
     @DeleteMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete (@PathVariable Long id) {
+    public void delete(@PathVariable Long id) {
         clienteService.delete(id);
     }
-    
+
 }
